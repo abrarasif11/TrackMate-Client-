e73b1df349f6578d0eb6f288974bc52ea9108ddc
abrarasif11 <faasif929@gmail.com>
2025-09-14 22:25:28 +0600

    shared logo

---
+++ src/Shared/TrackMateLogo/TrackMateLogo.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import logo from "../../assets/banner/logo.png";
+const TrackMateLogo = () => {
+  return (
+    <div>
+      <img src={logo} />
+    </div>
+  );
+};
+
+export default TrackMateLogo;
