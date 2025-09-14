b77a7055930239dfed70868bd97ade460a1008e9
abrarasif11 <faasif929@gmail.com>
2025-09-14 22:09:48 +0600

    navber and footer complete

---
+++ src/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import { Link, NavLink } from "react-router";
+
+const Navbar = () => {
+  const links = (
+    <>
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="/myApplication">My Application</NavLink>
+      </li>
+      <li>
+        <NavLink to="/createJob">Create Job</NavLink>
+      </li>
+      <li>
+        <NavLink to="/allJobs">All Jobs</NavLink>
+      </li>
+      <li>
+        <NavLink to="/myPostedJobs">My Posted Jobs</NavLink>
+      </li>
+    </>
+  );
+  return (
+    <div className="navbar max-w-7xl mx-auto bg-base-100 ">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+          >
+            {links}
+          </ul>
+        </div>
+        {/* <div className="flex  items-center">
+          <Link
+            to="/"
+            className=" font-josefin font-semibold normal-case text-2xl"
+          >
+            <img className="w-[55px]" src={headerPhoto} alt="" />
+          </Link>
+        </div> */}
+
+        <Link to="/" className="btn btn-ghost text-xl text-[#4167F0]">
+          Track Mate
+        </Link>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{links}</ul>
+      </div>
+      <div className="navbar-end">
+        <Link className="mr-3 underline text-[#4167F0]" to="/register">
+          Register
+        </Link>
+        <Link to="/signin">
+          <button className="btn bg-[#4167F0] text-white">Log In</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
