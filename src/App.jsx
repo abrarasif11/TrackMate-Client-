cb2d246d4cf378e72513c47d73958fef208f6866
abrarasif11 <faasif929@gmail.com>
2025-09-14 21:57:18 +0600

    router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <h2 className='text-center text-4xl'>Track Mate</h2>
     </>
   )
 }
