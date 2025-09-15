04120e90dce6245443eee1f3cac992cde46f4dad
abrarasif11 <faasif929@gmail.com>
2025-09-15 20:43:03 +0600

    auth context

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,13 @@ import "aos/dist/aos.css";
 AOS.init();
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Routes/Router.jsx";
+import AuthProvider from "./Context/AuthContext/AuthProvider.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <div className="font-urbanist max-w-[1650px] mx-auto">
-      <RouterProvider router={router} />
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
     </div>
   </StrictMode>
 );
