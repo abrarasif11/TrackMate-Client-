dcc2e8c088c0fb1f39ac2f517447b6cea203c926
abrarasif11 <faasif929@gmail.com>
2025-09-15 18:30:41 +0600

    login page

--- src/Layout/AuthLayout/AuthLayout.jsx
+++ src/Layout/AuthLayout/AuthLayout.jsx
@@ -5,17 +5,26 @@ import TrackMateLogo from "../../Shared/TrackMateLogo/TrackMateLogo";
 
 const AuthLayout = () => {
   return (
-    <div className="p-12 bg-base-200 ">
-      <div>
-        <TrackMateLogo />
-      </div>
-      <div className="hero-content flex-col lg:flex-row-reverse">
-        <div className="flex-1">
-          <img src={authImg} className="max-w-sm rounded-lg shadow-2xl" />
+    <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2 bg-[#FAFCF6]">
+      {/* Left Section */}
+      <div className="flex flex-col px-12 py-8">
+        {/* Logo */}
+        <div className="mb-8">
+          <TrackMateLogo />
         </div>
-        <div className="flex-1">
+        {/* Outlet for login/register */}
+        <div className="flex items-center justify-center flex-1">
           <Outlet />
         </div>
+      </div>
+
+      {/* Right Illustration */}
+      <div className="hidden lg:flex items-center justify-center bg-[#FAFCF6]">
+        <img
+          src={authImg}
+          alt="Auth Illustration"
+          className="max-w-lg w-full"
+        />
       </div>
     </div>
   );
