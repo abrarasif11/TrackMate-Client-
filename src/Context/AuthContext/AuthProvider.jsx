b5907e4d034d9834297d2afc7c68d3a1ffad177d
abrarasif11 <faasif929@gmail.com>
2025-09-15 21:37:03 +0600

    register connected with authContext

--- src/Context/AuthContext/AuthProvider.jsx
+++ src/Context/AuthContext/AuthProvider.jsx
@@ -1,15 +1,50 @@
-import React from 'react'
-import AuthContext from './AuthContext'
+import React, { useEffect, useState } from "react";
+import { auth } from "../../firebase/firebase.init"; // ✅ fixed import
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { AuthContext } from "./AuthContext";
 
-const AuthProvider = ({children}) => {
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+
+    return () => unSubscribe();
+  }, []);
+
   const authInfo = {
+    createUser,
+    signIn,
+    user,
+    loading,
+    logOut,
+  };
 
-  }
-  return (
-    <AuthContext value={authInfo}>
-      {children}
-    </AuthContext>
-  )
-}
+  return <AuthContext value={authInfo}>{children}</AuthContext>;
+};
 
-export default AuthProvider
+export default AuthProvider;
