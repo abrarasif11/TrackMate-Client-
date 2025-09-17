840138069354c01d3914c08800244648e7b62ddf
abrarasif11 <faasif929@gmail.com>
2025-09-17 14:34:20 +0600

    setup tanstack query

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,21 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 AOS.init();
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Routes/Router.jsx";
 import AuthProvider from "./Context/AuthContext/AuthProvider.jsx";
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <div className="font-urbanist max-w-[1650px] mx-auto">
-      <AuthProvider>
-        <RouterProvider router={router} />
-      </AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <RouterProvider router={router} />
+        </AuthProvider>
+      </QueryClientProvider>
     </div>
   </StrictMode>
 );
