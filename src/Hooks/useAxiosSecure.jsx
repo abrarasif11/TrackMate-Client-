f34d28db471a9f6114ff775b8aaedc1acd3eefd5
abrarasif11 <faasif929@gmail.com>
2025-09-20 11:50:05 +0600

    Axios interceptor

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,9 +1,20 @@
 import axios from "axios";
+import useAuth from "../../hooks/useAuth";
 
 const axiosSecure = axios.create({
   baseURL: `http://localhost:5000`,
 });
 const useAxiosSecure = () => {
+  const { user } = useAuth();
+  axiosSecure.interceptors.request.use(
+    (config) => {
+      config.headers.Authorization = `Bearer ${user.accessToken}`;
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
   return axiosSecure;
 };
 
