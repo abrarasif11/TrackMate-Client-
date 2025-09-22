3cf043ee6f978cbc617e8edb3672854cb6e2803f
abrarasif11 <faasif929@gmail.com>
2025-09-22 14:47:41 +0600

    Protect Admin api

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,11 +1,13 @@
 import axios from "axios";
 import useAuth from "../Hooks/useAuth";
+import { useNavigate } from "react-router";
 
 const axiosSecure = axios.create({
   baseURL: `http://localhost:5000`,
 });
 const useAxiosSecure = () => {
-  const { user } = useAuth();
+  const navigate = useNavigate();
+  const { user, logOut } = useAuth();
   axiosSecure.interceptors.request.use(
     (config) => {
       config.headers.Authorization = `Bearer ${user.accessToken}`;
@@ -13,6 +15,25 @@ const useAxiosSecure = () => {
     },
     (error) => {
       return Promise.reject(error);
+    }
+  );
+
+  axios.interceptors.response.use(
+    (res) => {
+      return res;
+    },
+    (err) => {
+      const status = err.status;
+      if (status === 403) {
+        navigate("/forbidden");
+      } else if (status === 401) {
+        logOut()
+          .then(() => {
+            navigate("/login");
+          })
+          .catch(() => {});
+      }
+      return Promise.reject(err);
     }
   );
   return axiosSecure;
